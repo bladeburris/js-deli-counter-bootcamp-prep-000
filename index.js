27c284549593cdfb944ea4efedf2a590c8c0f8b1
blade burris <bladeburris@gmail.com>
2018-04-17 16:31:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeliLine = []
 
 function takeANumber(katzDeliLine, newName) {
   katzDeliLine.push(newName)
-  var index = katzDeliLine.indexOf(newName)
+  var index = katzDeliLine.indexOf(newName) + 1
   return `Welcome, ${newName}. You are number ${index} in line.`
 }
